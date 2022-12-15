e418c83de16cec9f0d47c9403e1f87c5255a3a3c
Malek Bouneb <malek.bouneb3@gmail.com>
2022-12-15 12:31:40 +0100

    update App.js

---
+++ screens/PaymentScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function PaymentScreen() {
+ return (
+   <View style={styles.container}>
+     <Text>PaymentScreen</Text>
+   </View>
+ );
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+ },
+});
