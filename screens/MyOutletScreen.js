7bbb760fdfd05f46591aa5fa415450bf66d63283
Julien Deville <j.deville@protonmail.com>
2022-12-19 16:17:46 +0100

    tried to add average in my outlet

--- screens/MyOutletScreen.js
+++ screens/MyOutletScreen.js
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps'
 import * as Location from 'expo-location';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 
 export default function MyOutletScreen() {
@@ -12,6 +14,10 @@ export default function MyOutletScreen() {
   const [outletLongitude, setOutletLongitude] = useState()
   // Pour savoir si une prise a bien été trouvé
   const [validOutlet, setValidOutlet] = useState(false)
+  const [address, setAddress] = useState(null);
+  const [type, setType] = useState(null);
+  const [price, setPrice] = useState(null);
+  
 
   useEffect(() => {
     fetch(`https://voltify-backend.vercel.app/outlets/displayUserOutlet/${user.token}`)
@@ -20,9 +26,24 @@ export default function MyOutletScreen() {
         console.log('UseEffect data', data)
         setOutletLatitude(data.outlet.latitude);
         setOutletLongitude(data.outlet.longitude);
+        setAddress(data.outlet.address);
+        setType(data.outlet.type);
+        setPrice(data.outlet.price)
+
+
+//  TENTATIVE DE MOYENNE ECHOUE, bien tester le cas où le user aurai 0 votes.
+ /*        let vote = data.outlet.votes
+        console.log('vote', vote)
+        let averageVote
+        if(vote && vote.length > 0){
+          let averageVote = vote.reduce((a, b) => a + b, 0) / vote.length;
+          averageVote = averageVote.toFixed(1);
+          console.log('averageVote', averageVote);
+        } else averageVote = '(-)'
+
         if (data.result) {
           setValidOutlet(true)
-        }
+        } */
       });
   }, []);
   console.log('outletLatitude for the win', outletLatitude)
@@ -45,8 +66,26 @@ export default function MyOutletScreen() {
   return (
     <View style={styles.container}>
       <MapView style={styles.map} region={currentPosition}>
-        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="black" />}
+        {currentPosition && <Marker coordinate={currentPosition} title="My position" pinColor="#0FCCA7" />}
       </MapView>
+      <View style={styles.infoCardView}>
+        <View style= {styles.textWrap}>
+          <View style={{margin: 10,}}>
+              <Text>{type}</Text>
+              <Text>{address}</Text>
+              <Text>{price}</Text>
+          </View>
+          <View>
+            <Text style={{margin: 10}}>Vote Average</Text>
+          </View>
+        </View>
+          <TouchableOpacity style={styles.button}>
+            <Text style={{color: 'white'}}>Add outlet</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button2}>
+            <Text style={{color: 'white'}}> Delete </Text>
+          </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -59,5 +98,40 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1,
-  }
+  },
+ infoCardView: {
+    justifyContent: 'space-between', backgroundColor: 'white',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: "100%",
+    height: 180,
+    marginBottom: 20
+  },
+  button: {
+    alignItems: 'center',
+    paddingTop: 8,
+    width: '80%',
+    height: 30,
+    backgroundColor: '#0FCCA7',
+    borderRadius: 10,
+    marginBottom: 0,
+},
+button2: {
+  alignItems: 'center',
+  paddingTop: 8,
+  width: '80%',
+  height: 30,
+  backgroundColor: '#EE1B42',
+  borderRadius: 10,
+  marginBottom: 10,
+},
+textWrap: {
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  width: "100%",
+  alignItems: 'center'
+}
 });
+
+
+
